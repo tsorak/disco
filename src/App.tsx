79eee3bee8240ada477ffc51c2660ef3e30abb76
tsorak <karost0249@gmail.com>
2022-11-18 10:24:42 +0100

    Add Hello World example

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import type { Component } from "solid-js";
 const App: Component = () => {
 	return (
 		<>
-			<div class="h-screen bg-[#141414] flex justify-center items-center">
-				<h1 class="text-white text-4xl">Hello World!</h1>
+			<div class="h-screen dark:bg-[#141414] flex justify-center items-center">
+				<h1 class="HelloWorld">Hello World!</h1>
 			</div>
 		</>
 	);
