5848a6c1d047107dfc5d385dabce1a5803807726
tsorak <karost0249@gmail.com>
2022-11-18 09:45:37 +0100

    Add Tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import type { Component } from "solid-js";
 
 const App: Component = () => {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<div class="h-screen bg-[#141414] flex justify-center items-center">
+				<h1 class="text-white text-4xl">Hello World!</h1>
+			</div>
+		</>
+	);
 };
 
 export default App;
