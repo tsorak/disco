16689a07698058a435ad37ffe6949a1adb8ca958
tsorak <karost0249@gmail.com>
2022-11-19 15:57:45 +0100

    Add lightmode background palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,26 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
+				//bg-dark
 				"serverbar-bg-dark": "#202225",
 				"profile-bg-dark": "#292B2F",
 				"sidebar-bg-dark": "#2F3136",
 				"foreground-bg-dark": "#36393F",
-				"mention-bg": "#5865F24D",
 				"msgInput-bg-dark": "#40444B",
 				"interactable-bg-dark": "#202225",
 				"interactable-selected-bg-dark": "#4F545C99",
+				//text-dark
 				"primary-text-dark": "#DCDDDE",
 				"sidebar-text-dark": "#96989D",
 				"placeholder-text-dark": "#A3A6AA",
 				"interactable-text-dark": "#B9BBBE",
+				//bg-light
+				"serverbar-bg": "#E3E5E8",
+				"profile-bg": "#EBEDEF",
+				"sidebar-bg": "#F2F3F5",
+				"foreground-bg": "#FFFFFF",
+				//misc
+				"mention-bg": "#5865F24D",
 				"link-text": "#00AFF4",
 				"strike-text": "#ED4245",
 				"add-text": "#3BA55D",
